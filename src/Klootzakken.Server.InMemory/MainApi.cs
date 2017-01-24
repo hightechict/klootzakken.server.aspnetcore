a2a895c75be38e91136cfcf5eb9506b84671cbc2
Bart de Boer <bart.de.boer@divverence.com>
2017-01-24 21:35:32 +0100

    Created InMemory Server library

---
+++ src/Klootzakken.Server.InMemory/MainApi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Klootzakken.Server.ApiModel;
+
+namespace Klootzakken.Server.InMemory
+{
+    public class MainApi
+    {
+        ConcurrentDictionary<string,UserApi> _userApiDictionary = new ConcurrentDictionary<string, UserApi>();
+        public IObservable<IUserApi> GetUserApi(string userId)
+        {
+            //return _userApiDictionary.GetOrAdd(userId, id => new UserApi());
+            throw new NotImplementedException();
+        }
+    }
+}
