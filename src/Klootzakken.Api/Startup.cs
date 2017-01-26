5889b0a22790b01e9c70661c4ed8f9d74da2c8f3
Bart de Boer <bart.de.boer@divverence.com>
2017-01-26 11:50:52 +0100

    Fix: Added back ConfigureService / AddMvc

--- src/Klootzakken.Api/Startup.cs
+++ src/Klootzakken.Api/Startup.cs
@@ -36,6 +36,12 @@ namespace Klootzakken.Api
 
         public IConfigurationRoot Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
