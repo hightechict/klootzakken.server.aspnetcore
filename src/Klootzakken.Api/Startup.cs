0b8d0453e263f4cdd612ba990eef34f2fa213da5
Bart de Boer <bart.de.boer@divverence.com>
2017-01-26 10:32:43 +0100

    Removed ConfigureServices

--- src/Klootzakken.Api/Startup.cs
+++ src/Klootzakken.Api/Startup.cs
@@ -36,32 +36,11 @@ namespace Klootzakken.Api
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc(options =>
-            {
-                options.SslPort = 44376;
-                //options.Filters.Add(new RequireHttpsAttribute());
-            });
-        }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
-
-            if (env.IsDevelopment())
-            {
-                //app.UseDeveloperExceptionPage();
-//                app.UseBrowserLink();
-            }
-            else
-            {
-                //app.UseExceptionHandler("/Home/Error");
-                
-            }
 
             ConfigureAuth(app);
 
