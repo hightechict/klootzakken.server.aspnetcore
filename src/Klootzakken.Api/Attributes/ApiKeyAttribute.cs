3b596622430f0212c0feaf90f7209c7ec9995f01
Bart de Boer <bart.de.boer@divverence.com>
2021-03-30 15:42:37 +0200

    Improvements to ApiKeyAttribute

--- src/Klootzakken.Api/Attributes/ApiKeyAttribute.cs
+++ src/Klootzakken.Api/Attributes/ApiKeyAttribute.cs
@@ -8,10 +8,19 @@ using System.Threading.Tasks;
 
 namespace Klootzakken.Api.Attributes
 {
+    /// <summary>
+    /// Attribute that check API keys and generates a User
+    /// </summary>
     [AttributeUsage(validOn: AttributeTargets.Class)]
     public class ApiKeyAttribute : Attribute, IAsyncActionFilter
     {
         private const string APIKEYNAME = "playerId";
+        /// <summary>
+        /// Check playerId presence in request, maps to User
+        /// </summary>
+        /// <param name="context">the ActionContext</param>
+        /// <param name="next">the next in the request pipe</param>
+        /// <returns></returns>
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             if (!context.HttpContext.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
@@ -21,12 +30,24 @@ namespace Klootzakken.Api.Attributes
                     context.Result = new ContentResult()
                     {
                         StatusCode = 401,
-                        Content = "Api Key was not provided"
+                        Content = "Please provide &playerId=.... or playerId: .... Header"
                     };
                     return;
                 }
             }
 
+            var apiKey = extractedApiKey.ToString();
+
+            if (apiKey.Length < 5)
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 400,
+                    Content = "Provided playerId too short"
+                };
+                return;
+            }
+            
             //var appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
             /*
             var apiKey = appSettings.GetValue<string>(APIKEYNAME);
@@ -42,7 +63,7 @@ namespace Klootzakken.Api.Attributes
             }
             */
             var userId = extractedApiKey;
-            var userName = "Naam";
+            var userName = extractedApiKey;
             context.HttpContext.User = new ClaimsPrincipal(new[] { new ClaimsIdentity(
                 new[] { new Claim(ClaimTypes.NameIdentifier,userId),
                         new Claim(ClaimTypes.Name,userName)}) });
